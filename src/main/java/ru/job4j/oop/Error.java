7cbc4c90a1b7003d16c06c9c44f1bb2494b5f611
Yamijke <serhei.knyazev@gmail.com>
2023-03-12 20:10:32 +0300

    Конструктор [#173889 #399528]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -6,19 +6,28 @@ public class Error {
     private int status;
     private String message;
 
-    public void printInfo(){
-        System.out.println("Активная?: " + active);
-        System.out.println("Код ошибки: " + status);
-        System.out.println("Сообщение: " + message);
+    public Error() {
+        this.active = false;
+        this.status = 0;
+        this.message = "";
     }
+
     public Error(boolean active, int status, String message) {
         this.active = active;
         this.status = status;
         this.message = message;
     }
+    public void printInfo(){
+        System.out.println("Активная?: " + active);
+        System.out.println("Код ошибки: " + status);
+        System.out.println("Сообщение: " + message);
+    }
 
     public static void main(String[] args) {
         Error err = new Error(true, 404, "Страница не найдена");
         err.printInfo();
+
+        Error err1 = new Error();
+        err1.printInfo();
     }
 }
