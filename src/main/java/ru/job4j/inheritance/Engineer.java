28f9aa0f04b69232e966863e907917e4e844994a
Yamijke <serhei.knyazev@gmail.com>
2023-03-24 17:54:19 +0300

    checkstyle fixes

--- src/main/java/ru/job4j/inheritance/Engineer.java
+++ src/main/java/ru/job4j/inheritance/Engineer.java
@@ -1,6 +1,5 @@
 package ru.job4j.inheritance;
 
-
 public class Engineer extends Profession {
     private int experience;
 
--- src/main/java/ru/job4j/pojo/License.java
+++ src/main/java/ru/job4j/pojo/License.java
@@ -43,7 +43,9 @@ public class License {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+        if (this == o) {
+            return true;
+        }
         if (o == null || getClass() != o.getClass()) return false;
         License license = (License) o;
         return Objects.equals(owner, license.owner)
