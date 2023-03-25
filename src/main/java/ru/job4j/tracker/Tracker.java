9c73ceacc0d50954c5a0ce1b161351b6d7f1bc67
Yamijke <serhei.knyazev@gmail.com>
2023-03-25 13:17:38 +0300

    Метод удаления заявки Tracker.delete [#211749 #403631]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -61,7 +61,6 @@ public class Tracker {
         if (index == -1) {
             return false;
         }
-        items[indexOf(id)] = null;
         System.arraycopy(items, index + 1, items, index, size - index - 1);
         items[size - 1] = null;
         size--;
