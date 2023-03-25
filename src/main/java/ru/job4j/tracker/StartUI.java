d95d82f98acd1433aa2cf6cb3fb6aff8f0521e25
Yamijke <serhei.knyazev@gmail.com>
2023-03-25 15:41:59 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,5 +10,8 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String formattedDateTime = created.format(formatter);
         System.out.println("Item was created on " + formattedDateTime);
+
+        Item item1 = new Item(1, "name1");
+        System.out.println(item1);
     }
 }
