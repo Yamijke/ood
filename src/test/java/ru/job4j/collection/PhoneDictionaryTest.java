f2d167769af5197629ebeb2e6e230a6aa4822e52
Yamijke <serhei.knyazev@gmail.com>
2023-04-18 18:18:14 +0300

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/test/java/ru/job4j/collection/PhoneDictionaryTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenNotFound() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Name", "Surname", "123456", "Hell"));
+        ArrayList<Person> persons = phones.find("987");
+        assertThat(persons).isEmpty();
+    }
+}
