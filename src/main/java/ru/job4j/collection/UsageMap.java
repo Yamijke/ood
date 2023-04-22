29a843ad271aaf7e5128e384ab8f25a5b5e4b6c5
Yamijke <serhei.knyazev@gmail.com>
2023-04-22 14:22:49 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("qwerty@qwerty.com", "Qwerty Qwertovich");
+        for (String key : hashMap.keySet()) {
+            String value = hashMap.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
