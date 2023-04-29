f623004d323f7f3847944cf84ec44a2d961ee3b0
Yamijke <serhei.knyazev@gmail.com>
2023-04-29 15:58:11 +0300

    5. PriorityQueue [#504944]

---
+++ src/main/java/ru/job4j/queue/ScheduleTask.java
@@ -0,0 +1,27 @@
+package ru.job4j.queue;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class ScheduleTask {
+    private PriorityQueue<Task> queue;
+
+    public ScheduleTask(Comparator<Task> comparator) {
+        this.queue = new PriorityQueue<>(comparator);
+    }
+
+    public void addTask(Task task) {
+        queue.offer(task);
+    }
+
+    public Task readTask() {
+        return queue.peek();
+    }
+
+    public Task getTask() {
+        if (!queue.isEmpty()) {
+            return queue.poll();
+        }
+        return null;
+    }
+}
