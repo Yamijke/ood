c8e05d582683d314e4535e4c849f5ae6430db23b
Yamijke <serhei.knyazev@gmail.com>
2023-05-16 14:17:21 +0300

    6. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+public class Profile {
+    private Address address;
+
+    public Profile(Address adress) {
+        this.address = adress;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+}
