b448a7e12af363959357c742c8d341d6125bd87b
Yamijke <serhei.knyazev@gmail.com>
2023-05-18 18:16:25 +0300

    17. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 /**
  * Класс содержит функционал простейшей вариации банковской системы
+ *
  * @author Sergey Knyazev
  * @version 1.0
  */
@@ -19,6 +20,7 @@ public class BankService {
 
     /**
      * Метод принимает на вход нового User и добавляет в HashMap
+     *
      * @param user данные, по которым производится поиск User
      */
     public void addUser(User user) {
@@ -27,6 +29,7 @@ public class BankService {
 
     /**
      * Метод удаляет User из HashMap
+     *
      * @param passport данные, по которым производится поиск User
      * @return возвращает true если User найден по полю пасспорт
      */
@@ -37,8 +40,9 @@ public class BankService {
     /**
      * Метод добавляет User новый счет.
      * Поиск User производится по passport
+     *
      * @param passport данные, по которым производится поиск User
-     * @param account данные, по которым производится внесение нового счета
+     * @param account  данные, по которым производится внесение нового счета
      */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -52,34 +56,34 @@ public class BankService {
 
     /**
      * Метод осуществляет поиск User по его полю Passport
+     *
      * @param passport данные, по которым производится поиск User
      * @return возвращает null, если поиск не нашел введенные данные
      */
     public User findByPassport(String passport) {
-        for (User foundUser : users.keySet()) {
-            if (passport.equals(foundUser.getPassport())) {
-                return foundUser;
-            }
-        }
-        return null;
+        return users.keySet()
+                .stream()
+                .filter(u -> u.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     /**
      * Метод осуществляет поиск User по ревизитам его счета
      * Если юзер существует и счет не найден, возврящается null
-     * @param passport данные, по которым производится поиск User
+     *
+     * @param passport  данные, по которым производится поиск User
      * @param requisite данные, по которым производится поиск Account
      * @return возвращает null, если по заданным параметрам поиск не дал результатов
      */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
-            List<Account> accounts = users.get(user);
-            for (Account currentAcc : accounts) {
-                if (currentAcc.getRequisite().equals(requisite)) {
-                    return currentAcc;
-                }
-            }
+            return users.get(user)
+                    .stream()
+                    .filter(u -> u.getRequisite().equals(requisite))
+                    .findFirst()
+                    .orElse(null);
         }
         return null;
     }
@@ -87,11 +91,12 @@ public class BankService {
     /**
      * Данный метод осуществляет перевод денежных средств с одного счета
      * на другой счет.
-     * @param srcPassport поиск User, со счета которого будет перевод
-     * @param srcRequisite поиск Account, с которого будет перевод
-     * @param destPassport поиск User, на счет которого будет перевод
+     *
+     * @param srcPassport   поиск User, со счета которого будет перевод
+     * @param srcRequisite  поиск Account, с которого будет перевод
+     * @param destPassport  поиск User, на счет которого будет перевод
      * @param destRequisite поиск Account, на который будет перевод
-     * @param amount сумма к переводу
+     * @param amount        сумма к переводу
      * @return возвращает false если на счете недостаточно средств
      */
     public boolean transferMoney(String srcPassport, String srcRequisite,
@@ -109,6 +114,7 @@ public class BankService {
 
     /**
      * Метод служит выводу счетов определенного User
+     *
      * @param user вывод счетов по User
      * @return возвращает коллекцию счетов User
      */
