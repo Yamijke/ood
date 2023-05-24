c6d3775b6b308efb6a703d75c8f97d2b89061f23
Yamijke <serhei.knyazev@gmail.com>
2023-05-24 18:09:50 +0300

    3. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/lasttask/Subject.java
@@ -0,0 +1,5 @@
+package ru.job4j.stream.lasttask;
+
+
+public record Subject(String name, int score) {
+}
---
+++ src/main/java/ru/job4j/stream/lasttask/Tuple.java
@@ -0,0 +1,4 @@
+package ru.job4j.stream.lasttask;
+
+public record Tuple(String name, double score) {
+}
