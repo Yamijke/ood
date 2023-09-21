b75da5659a23db70667be0e886fe4e4d1a801fa7
Yamijke <serhei.knyazev@gmail.com>
2023-09-21 16:57:54 +0300

    3. Liquibase. Интеграционные тесты. [#504894 #446603]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -2,11 +2,8 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTest {
 
