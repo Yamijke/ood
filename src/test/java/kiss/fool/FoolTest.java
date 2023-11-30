54b3565adfc437072a0c90f6629477cd11e43559
Yamijke <serhei.knyazev@gmail.com>
2023-11-30 19:12:18 +0300

    2. Принципы Kiss, Dry и Yagni [#238813 #463561]

--- src/test/java/kiss/fool/FoolTest.java
+++ src/test/java/kiss/fool/FoolTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class FoolTest {
 
     @Test
-    public void answers () {
+    public void answers() {
         assertEquals("1", Fool.ret(1), "Ошибка при 1");
         assertEquals("Fizz", Fool.ret(3), "Ошибка при 3");
         assertEquals("Buzz", Fool.ret(5), "Ошибка при 5");
