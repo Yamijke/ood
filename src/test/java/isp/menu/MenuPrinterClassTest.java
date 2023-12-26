fb5df857f0e9feab46dba3f192155f9330336bdc
Yamijke <serhei.knyazev@gmail.com>
2023-12-26 13:47:17 +0300

    1. Создать меню. [#4748 #468645]

--- src/test/java/isp/menu/MenuPrinterClassTest.java
+++ src/test/java/isp/menu/MenuPrinterClassTest.java
@@ -27,9 +27,12 @@ public class MenuPrinterClassTest {
     public void testPrint() {
         MenuPrinterClass printer = new MenuPrinterClass();
         SimpleMenu menu = new SimpleMenu();
-        menu.add(null, "Item 1", () -> {});
-        menu.add("Item 1", "Subitem 1.1", () -> {});
-        menu.add("Item 1", "Subitem 1.2", () -> {});
+        menu.add(null, "Item 1", () -> {
+        });
+        menu.add("Item 1", "Subitem 1.1", () -> {
+        });
+        menu.add("Item 1", "Subitem 1.2", () -> {
+        });
         printer.print(menu);
         String expectedOutput = "Item 1\n    Subitem 1.1\n    Subitem 1.2\n";
         String actualOutput = outContent.toString().replace("\r\n", "\n");
